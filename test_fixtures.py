bac3d1537d6fa2782a6ee357a9bccb5bcfa00740
SamSDET <abdulsamad453@outlook.com>
2025-12-26 21:12:11 +0530

    conftest

--- test_fixtures.py
+++ test_fixtures.py
@@ -1,1 +1,10 @@
-print('hi')
+#fixtures
+import pytest
+
+@pytest.fixture(scope="function")#if it is module it will run once for entire test file if it is function it will run before each test case in file
+def preWork():
+    print("I am a fixture")
+
+def test_initialcheck(preWork):
+    print("Initial check passed.")
+
---
+++ thirdtest.py
@@ -0,0 +1,2 @@
+def test_thirdcheck(preSetUpWork):
+    print("Third check passed.")
