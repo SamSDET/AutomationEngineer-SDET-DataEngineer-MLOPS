0bc9766f689902cf0e4c15018af2562f2254f003
SamSDET <abdulsamad453@outlook.com>
2025-12-31 23:47:57 +0530

    "playwrightassertions"

--- Playwright/test_FirstPlaywright.py
+++ Playwright/test_FirstPlaywright.py
@@ -1,5 +1,5 @@
 import time
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 def test_playwright_function(playwright):
     browser = playwright.chromium.launch(headless=False)
     context1 = browser.new_context()
@@ -10,10 +10,18 @@ def test_playwright_function(playwright):
 def test_another_function(page:Page):
     page.goto("https://rahulshettyacademy.com")
 
-def test_corelocators(page:Page):
+def test_corelocators(playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context1 = browser.new_context()
+    page =context1.new_page()
     page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     page.get_by_label("Username:").fill("rahulshettyacademy")
-    page.get_by_label("Password:").fill("learning")
+    page.get_by_label("Password:").fill("learning1")
     page.get_by_role("combobox").select_option("teach")
-    time.sleep(5)
+    page.locator("#terms").check()
+    #time.sleep(5)
+    #page.get_by_role("link", name="terms and conditions").click()
+    #id it becomes CSS .classname becomes CSS
+    page.locator("#signInBtn").click()
+    expect(page.get_by_text("Incorrect username/password.")).to_be_visible()
 
