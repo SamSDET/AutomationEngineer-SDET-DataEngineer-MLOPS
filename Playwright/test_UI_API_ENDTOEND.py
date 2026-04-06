638f0dbda25da9e5becd2e1f21dd7491b9178612
SamSDET <abdulsamad453@outlook.com>
2026-04-06 17:16:37 +0530

    testinf 3

--- Playwright/test_UI_API_ENDTOEND.py
+++ Playwright/test_UI_API_ENDTOEND.py
@@ -2,7 +2,7 @@ from playwright.sync_api import Playwright
 
 def test_e2e_API_UI(playwright:Playwright):
     browser = playwright.chromium.launch(headless=False)
-    context = tbrowser.new_context()
+    context = browser.new_context()
     page = context.new_page()
     page.goto("htps://rahulshettyacademy.com/client/")
 
