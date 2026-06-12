34f05a6963e1d617a5acff2f59c2d0b4c8c332bf
SamSDET <abdulsamad453@outlook.com>
2026-06-12 12:57:17 +0530

    API_UI_Demo

--- Playwright/test_UI_API_ENDTOEND.py
+++ Playwright/test_UI_API_ENDTOEND.py
@@ -4,7 +4,10 @@ def test_e2e_API_UI(playwright:Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("htps://rahulshettyacademy.com/client/")
-
-
+    page.goto("https://rahulshettyacademy.com/client/#/dashboard/dash")
+    page.locator("#userEmail").fill("abdulsamad453@outlook.com")
+    page.get_by_placeholder("enter your passsword").fill("!Jun26@honWRV")
+    page.locator("#login").click()
     
+   
+
