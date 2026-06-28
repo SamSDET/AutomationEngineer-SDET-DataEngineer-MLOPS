a213ecf389458cafa379ee1919983b2aefc3c19b
SamSDET <abdulsamad453@outlook.com>
2026-06-28 22:41:15 +0530

    spell check by PyCharm

--- Playwright/test_network_1.py
+++ Playwright/test_network_1.py
@@ -1,9 +1,9 @@
 from playwright.sync_api import Playwright
 
-fakepayloadresponse = {"data":[],"message":"No Orders"}
+irresponsibility = {"data":[], "message": "No Orders"}
 
 def intercept_response(route):
-    route.fulfill( json = fakepayloadresponse)
+    route.fulfill(json = irresponsibility)
 
 def test_network_1(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
@@ -12,7 +12,7 @@ def test_network_1(playwright: Playwright):
     driver1.goto("https://rahulshettyacademy.com/client/#/dashboard/dash")
     driver1.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/*", intercept_response)
     driver1.locator("#userEmail").fill("abdulsamad453@outlook.com")
-    driver1.get_by_placeholder("enter your passsword").fill("!Jun26@honWRV")
+    driver1.get_by_placeholder("enter your password").fill("!Jun26@honWRV")
     driver1.locator("#login").click()
     driver1.locator("button[routerlink='/dashboard/myorders']").click()
     order_text=driver1.locator(".mt-4").text_content() 
